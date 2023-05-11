ae8447ac0d301aaf6ca8cc23768eead6b472ad01
Frances Leigh <fleighbaay@gmail.com>
2023-05-11 23:04:02 +0800

    Pseudocode

---
+++ Exception-Handled-Calculator.py
@@ -0,0 +1,12 @@
+# Create loop to repeat calculations and input writing
+
+# Ask user to choose operation
+# Ask user to input two numbers
+
+# Perform the calculation
+
+# Display the result of the calculation after raising errors
+
+# Handle exceptions based on the conditions met during the calculation
+
+# Ask user if they want to try again or exit the program
