f4b71248d9edfd9da4f132f8c273ec55d1ea73d3
Frances Leigh <fleighbaay@gmail.com>
2023-05-11 23:24:51 +0800

    Ask user if they want to try again

--- Exception-Handled-Calculator.py
+++ Exception-Handled-Calculator.py
@@ -1,11 +1,13 @@
 # Create loop to repeat calculations and input writing
 while True:
     try:
+        
         # Ask user to choose operatio
         operation = input("Choose an operation (+, -, *, /): ")
         # Ask user to input two numbers
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
+        
         # Perform the calculation
         if operation == '+':
             result = num1 + num2
@@ -20,12 +22,19 @@ while True:
                 result = num1 / num2
         else:
             raise ValueError("Invalid operation selected.")
+        
         # Display the result of the calculation after raising errors
         print("Result:", result)
+    
     # Handle exceptions based on the conditions met during the calculation
     except ValueError as error:
         print("Error:", error)
 
     except ZeroDivisionError as error:
         print("Error:", error)
-# Ask user if they want to try again or exit the program
+
+    # Ask user if they want to try again or exit the program
+    trial = input("Do you want to try again? (Y/N): ")
+    if trial.upper() == 'N':
+        print("Thank you!")
+        break
