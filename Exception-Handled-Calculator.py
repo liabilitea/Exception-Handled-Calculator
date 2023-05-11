55a5ebb93c57fdf3dcb40688b1dfc66473ff8c63
Frances Leigh <fleighbaay@gmail.com>
2023-05-11 23:13:23 +0800

    Calculate and print result

--- Exception-Handled-Calculator.py
+++ Exception-Handled-Calculator.py
@@ -6,10 +6,22 @@ while True:
         # Ask user to input two numbers
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
-# Perform the calculation
-
-# Display the result of the calculation after raising errors
-
+        # Perform the calculation
+        if operation == '+':
+            result = num1 + num2
+        elif operation == '-':
+            result = num1 - num2
+        elif operation == '*':
+            result = num1 * num2
+        elif operation == '/':
+            if num2 == 0:
+                raise ZeroDivisionError("Cannot divide by zero.")
+            else:
+                result = num1 / num2
+        else:
+            raise ValueError("Invalid operation selected.")
+        # Display the result of the calculation after raising errors
+        print("Result:", result)
 # Handle exceptions based on the conditions met during the calculation
 
 # Ask user if they want to try again or exit the program
