6d403c8dbb7000f624bc217607b02dbd1c282b07
Frances Leigh <fleighbaay@gmail.com>
2023-05-12 00:11:42 +0800

    Pseudocode for UI, modify calculation

--- Exception-Handled-Calculator.py
+++ Exception-Handled-Calculator.py
@@ -40,47 +40,48 @@ class Calculator:
         self.calculate_button = tk.Button(ui, text="Calculate", command=self.calculate)
         self.calculate_button.pack(pady=10)
 
-
-
-# WAG MUNA GALAWIN T.T
 #Define calculate method 
-# Create loop to repeat calculations and input writing
-while True:
-    try:
+    def calculate(self):
+        try:
         
-        # Ask user to choose operatio
-        operation = input("Choose an operation (+, -, *, /): ")
-        # Ask user to input two numbers
-        num1 = float(input("Enter the first number: "))
-        num2 = float(input("Enter the second number: "))
+            # Ask user to choose operatio
+            operation = input("Choose an operation (+, -, *, /): ")
+            # Ask user to input two numbers
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
         
-        # Perform the calculation
-        if operation == '+':
-            result = num1 + num2
-        elif operation == '-':
-            result = num1 - num2
-        elif operation == '*':
-            result = num1 * num2
-        elif operation == '/':
-            if num2 == 0:
-                raise ZeroDivisionError("Cannot divide by zero.")
-            else:
+            # Perform the calculation
+            if operation == '+':
+                result = num1 + num2
+            elif operation == '-':
+                result = num1 - num2
+            elif operation == '*':
+                result = num1 * num2
+            elif operation == '/':
                 result = num1 / num2
-        else:
-            raise ValueError("Invalid operation selected.")
-        
-        # Display the result of the calculation after raising errors
+            else:        
+                raise ValueError("Invalid operation selected.")
+            
+        # Display the result in UI
         print("Result:", result)
     
-    # Handle exceptions based on the conditions met during the calculation
+    # Handle exceptions and display in UI
     except ValueError as error:
         print("Error:", error)
 
     except ZeroDivisionError as error:
         print("Error:", error)
 
-    # Ask user if they want to try again or exit the program
+    # Clear operation and number entry fields
+
+
+    # Ask user if they want to try again or exit using messagebox
     trial = input("Do you want to try again? (Y/N): ")
+    # If yes, clear result label and continue
     if trial.upper() == 'N':
         print("Thank you!")
         break
+    # If no, destroy UI and exit
+
+# Create root window and Calculator instance  
+# Start the main event loop 
